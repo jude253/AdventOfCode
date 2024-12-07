9c9e59a668e9927826b7de1890d21071a65bc8b8
Jude <judecapachietti@gmail.com>
2024-12-06 21:19:40 -0800

    Add code for day 7

--- src/advent_of_code/solutions/day_07.py
+++ src/advent_of_code/solutions/day_07.py
@@ -6,13 +6,64 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
+OPERATORS = ['||', '*', '+']
+
+
 class Day_07(Solution):
     def part_one(self):
-        print(self.input_file)
-        return
+        lines = self.input_file.split('\n')
+        answer_list = []
+        operands_list = []
+        for line in lines:
+            s = line.split(':')
+            answer_list.append(int(s[0]))
+            operands_list.append([int(x) for x in s[1].split()])
+        total_sum = 0
+        for answer, operands in zip(answer_list, operands_list):
+            stack = [(0, operands[0])]
+            while stack:
+                cur_index, cur_total = stack.pop()
+                if cur_index < len(operands) - 1:
+                    next_index = cur_index + 1
+                    next_val = operands[next_index]
+                    for operator in OPERATORS:
+                        if operator == '+':
+                            stack.append((next_index, cur_total+next_val))
+                        if operator == '*':
+                            stack.append((next_index, cur_total*next_val))
+                elif cur_index == len(operands) - 1 and cur_total == answer:
+                    total_sum += answer
+                    break
+        return total_sum
 
     def part_two(self):
-        return
+        lines = self.input_file.split('\n')
+        answer_list = []
+        operands_list = []
+        for line in lines:
+            s = line.split(':')
+            answer_list.append(int(s[0]))
+            operands_list.append([int(x) for x in s[1].split()])
+        total_sum = 0
+        for answer, operands in zip(answer_list, operands_list):
+            stack = [(0, operands[0])]
+            while stack:
+                cur_index, cur_total = stack.pop()
+                if cur_index < len(operands) - 1:
+                    next_index = cur_index + 1
+                    next_val = operands[next_index]
+                    for operator in OPERATORS:
+                        if operator == '||':
+                            stack.append((next_index, int(str(cur_total)+str(next_val))))
+                        if operator == '+':
+                            stack.append((next_index, cur_total+next_val))
+                        if operator == '*':
+                            stack.append((next_index, cur_total*next_val))
+                elif cur_index == len(operands) - 1 and cur_total == answer:
+                    total_sum += answer
+                    break
+
+        return total_sum
 
 
 if __name__ == "__main__":
