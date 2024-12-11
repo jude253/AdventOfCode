30bc014baa3eb94cfe29617d17dc889a0876c00a
Jude <judecapachietti@gmail.com>
2024-12-11 07:45:22 -0800

    Add code for day 11

--- src/advent_of_code/solutions/day_11.py
+++ src/advent_of_code/solutions/day_11.py
@@ -1,13 +1,46 @@
+from collections import defaultdict
+
 from advent_of_code.utils.daily_code_utils import Solution
+
+
+def process_stone(stone):
+    str_stone = str(stone)
+    len_stone = len(str_stone)
+    if stone == 0:
+        return [1]
+    elif len_stone % 2 == 0:
+        half = len(str_stone) // 2
+        stone1, stone2 = int(str_stone[:half]), int(str_stone[half:])
+        return [stone1, stone2]
+    else:
+        return [stone * 2024]
+
+
+def blink(stones_map):
+    new_stones_map = defaultdict(int)
+    for stone, count in stones_map.items():
+        new_stones = process_stone(stone)
+        for new_stone in new_stones:
+            new_stones_map[new_stone] += count
+    return new_stones_map
 
 
 class Day_11(Solution):
     def part_one(self):
-        print(self.input_file)
-        return
+        stones_map = defaultdict(int)
+        for stone in self.input_file.split():
+            stones_map[int(stone)] += 1
+        for _ in range(25):
+            stones_map = blink(stones_map)
+        return sum(list(stones_map.values()))
 
     def part_two(self):
-        return
+        stones_map = defaultdict(int)
+        for stone in self.input_file.split():
+            stones_map[int(stone)] += 1
+        for _ in range(75):
+            stones_map = blink(stones_map)
+        return sum(list(stones_map.values()))
 
 
 if __name__ == "__main__":
